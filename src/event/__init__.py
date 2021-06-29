eba909d2638beb221d698e6d508e9c9c9d4eb13b
yosiasm <yosiassuparno@gmail.com>
2021-06-29 20:46:11 +0700

    change init

--- src/event/__init__.py
+++ src/event/__init__.py
@@ -1,20 +1,20 @@
-from flask_socketio import SocketIO, emit, join_room, leave_room
-from flask import request
-import eventlet
-eventlet.monkey_patch()
-socketio = SocketIO(logger=True, engineio_logger=True)
+# from flask_socketio import SocketIO, emit, join_room, leave_room
+# from flask import request
+# import eventlet
+# eventlet.monkey_patch()
+# socketio = SocketIO(logger=True, engineio_logger=True)
 
-@socketio.on('connect')
-def connect():
-    print('connecting')
-    foo = request.args.get('satker_id')
-    join_room(foo)
-    emit('after connect', {'room': foo},room=foo)
+# @socketio.on('connect')
+# def connect():
+#     print('connecting')
+#     foo = request.args.get('satker_id')
+#     join_room(foo)
+#     emit('after connect', {'room': foo},room=foo)
 
-@socketio.on('disconnect')
-def disconnect():
-    foo = request.args.get('satker_id')
-    leave_room(foo)
-    emit('Client disconnected',{'room': foo},room=foo)
+# @socketio.on('disconnect')
+# def disconnect():
+#     foo = request.args.get('satker_id')
+#     leave_room(foo)
+#     emit('Client disconnected',{'room': foo},room=foo)
 
-from . import notifikasi
+# from . import notifikasi
